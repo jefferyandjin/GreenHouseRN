680d35d8dae5244ec198ae027f2e340c141d92dd
jeffery Liu <jefferyjobs@yahoo.ca>
2025-09-07 19:27:25 -0600

    fixed an environment config/build issue

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-  presets: [
-    "module:metro-react-native-babel-preset",
-    "@babel/preset-typescript",
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      // only add extra plugins if you really need them
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+    ],
+  };
 };
